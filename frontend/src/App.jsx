47964d03eb0010bb90ecdfb60139466a8932b481
m.bebars <66588352+mahmoud-bebars@users.noreply.github.com>
2024-09-22 07:24:35 +0300

    (fix): upadting BASE_URL

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar";
 import UserGrid from "./components/UserGrid";
 import { useState } from "react";
 
-export const BASE_URL = "http://localhost:5000";
+export const BASE_URL = "http://localhost:5000/api";
 
 export default function App() {
   const [users, setUsers] = useState([]);
