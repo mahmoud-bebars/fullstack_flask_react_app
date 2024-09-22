37ee4aa72cb3221ce342ab094cd86bb5d9e7e1e8
m.bebars <66588352+mahmoud-bebars@users.noreply.github.com>
2024-09-22 07:30:55 +0300

    (fix): fixing serve frontend function

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 # Import Dependencies
-from flask import Flask
+import os
+from flask import Flask, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
@@ -10,22 +11,32 @@ app = Flask(__name__)
 CORS(app)
 
 # DataBase Configuration
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///friend.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///friend.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 # Create Database Instance
 db = SQLAlchemy(app)
 
-# Set Application Root to /api
-app.config["APPLICATION_ROOT"] = "/api"
+frontend_folder = os.path.join(os.getcwd(), "..", "frontend")
+dist_folder = os.path.join(frontend_folder, "dist")
+
+
+# Serve Static  files from the dist_folder under the frontend directory
+@app.route("/", defaults={"filename": ""})
+@app.route("/<path:filename>")
+def index(filename):
+    if not filename:
+        filename = "index.html"
+    return send_from_directory(dist_folder, filename)
+
 
 # Import Routes
 import routes
 
 # Create Database Tables (if they don't already exist)
 with app.app_context():
-  db.create_all()
+    db.create_all()
 
 # Run the Application
-if __name__ == '__main__':
-  app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
